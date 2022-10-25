9f52b4c7e4fcba231f34b5ad5da9bbfde0e94d60
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-25 13:25:20 +0100

    Compare Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,9 +26,14 @@ const login = async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  // compare password
-
   if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
     throw new UnauthenticatedError("Invalid credentials");
   }
 
